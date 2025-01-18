7cdcd2d9b40e1fb7f2031072445fe340029a5542
jamshiddin <turdiev.jamshiddin@gmail.com>
2025-01-18 22:52:13 +0500

    updated and and some files to working dir

--- kubernetes/k8s-cluster/main.tf
+++ kubernetes/k8s-cluster/main.tf
@@ -1,11 +1,23 @@
+terraform {
+  required_version = ">= 1.3.2"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.61"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1" # Use only the region, not the availability zone
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "my-cluster"
-  cluster_version = "1.26"
-  subnets         = ["subnet-12345", "subnet-67890"]
-  vpc_id          = "vpc-12345"
+  version         = "20.24.0" # Specify the module version
+  cluster_name    = "my-cluster2"
+  cluster_version = "1.30"
+  subnet_ids      = ["subnet-0e3498f1143cda3b2", "subnet-070b06ba32474bd65"] # Corrected to subnet_ids
+  vpc_id          = "vpc-0884137077183ec41"
 }
